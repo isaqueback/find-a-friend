6b9abbf29d784d03b91cca539f3a8b2d0aae6214
Isaque Delfino <isa.quecosta00@gmail.com>
2023-11-20 13:35:25 +0000

    form validation with zod in login.tsx

--- src/app/login/org/page.tsx
+++ src/app/login/org/page.tsx
@@ -7,21 +7,46 @@ import fullLogoIllustration from '@/assets/full-logo.svg'
 import eyeOffIllustration from '@/assets/eye-off.svg'
 import eyeOnIllustration from '@/assets/eye-on.svg'
 import { useState } from 'react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const loginFormSchema = z.object({
+  email: z.string().email('Isto não é um e-mail válido.'),
+  password: z.string().min(4, 'A senha deve ter no mínimo 4 dígitos.'),
+})
+
+type LoginFormType = z.infer<typeof loginFormSchema>
 
 export default function Login() {
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
-  const [isPasswordFocus, setIsPasswordFocus] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormType>({
+    resolver: zodResolver(loginFormSchema),
+  })
+  const [password, setPassword] = useState({
+    isVisible: false,
+    isFocused: false,
+  })
 
-  function handlePasswordVisible() {
-    setIsPasswordVisible((state) => !state)
-  }
-
-  function handlePasswordFocus() {
-    setIsPasswordFocus((state) => {
-      console.log(!state)
-      return !state
+  function handleTogglePasswordVisibility() {
+    setPassword((state) => {
+      return { ...state, isVisible: !state.isVisible }
     })
   }
+
+  function handleFocusChange(isFocused: boolean) {
+    setPassword((state) => {
+      return { ...state, isFocused }
+    })
+  }
+
+  function onSubmit(data: LoginFormType) {
+    console.log(data)
+  }
+
   return (
     <div className="px-32 py-20 max-xl:p-4 flex w-full min-h-screen font-nunito justify-center">
       <div className="w-full max-w-[105rem] flex gap-32">
@@ -62,7 +87,11 @@ export default function Login() {
               height={195}
             />
           </div>
-          <form action="" className="flex flex-col gap-4">
+          <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div className="flex flex-col gap-2">
               <label
                 htmlFor="email"
@@ -71,32 +100,37 @@ export default function Login() {
                 Email
               </label>
               <input
+                {...register('email')}
                 type="email"
                 id="email"
                 placeholder="nome@email.com"
                 className="outline-0 bg-slate-50 rounded-xl border border-slate-300 p-5 text-sky-900 text-lg font-semibold focus:border-sky-900"
               />
+              {errors.email?.message && (
+                <small className="text-rose-500">{errors.email.message}</small>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label
-                htmlFor={isPasswordVisible ? 'text' : 'password'}
+                htmlFor={password.isVisible ? 'text' : 'password'}
                 className="text-sky-900 text-base font-semibold"
               >
                 Senha
               </label>
               <div className="flex items-center bg-slate-50 border-slate-300 rounded-tr-xl rounded-br-xl">
                 <input
-                  type={isPasswordVisible ? 'text' : 'password'}
+                  {...register('password')}
+                  type={password.isVisible ? 'text' : 'password'}
                   id="password"
                   placeholder="Coloque sua senha"
                   className="outline-0 p-5 bg-slate-50 rounded-xl border rounded-r-none border-r-0 text-sky-900 text-lg font-semibold flex-1 focus:border-sky-900"
-                  onFocus={handlePasswordFocus}
-                  onBlur={handlePasswordFocus}
+                  onFocus={() => handleFocusChange(true)}
+                  onBlur={() => handleFocusChange(false)}
                 />
                 <label
                   htmlFor="password"
                   className={`border border-l-0 rounded-tr-xl pr-5 rounded-br-xl cursor-text h-full flex item-center ${
-                    isPasswordFocus ? 'border-sky-900' : 'border-slate-300'
+                    password.isFocused ? 'border-sky-900' : 'border-slate-300'
                   }`}
                 >
                   <div className="flex cursor-pointer">
@@ -106,9 +140,9 @@ export default function Login() {
                       width={24}
                       height={24}
                       className={`opacity-50 ${
-                        isPasswordVisible ? '' : 'hidden'
+                        password.isVisible ? '' : 'hidden'
                       }`}
-                      onClick={handlePasswordVisible}
+                      onClick={handleTogglePasswordVisibility}
                     />
                     <Image
                       src={eyeOnIllustration}
@@ -116,18 +150,27 @@ export default function Login() {
                       width={24}
                       height={24}
                       className={`opacity-50 ${
-                        isPasswordVisible ? 'hidden' : ''
+                        password.isVisible ? 'hidden' : ''
                       }`}
-                      onClick={handlePasswordVisible}
+                      onClick={handleTogglePasswordVisibility}
                     />
                   </div>
                 </label>
               </div>
+              {errors.password?.message && (
+                <small className="text-rose-500">
+                  {errors.password.message}
+                </small>
+              )}
             </div>
             <div className="flex flex-col gap-4 mt-11">
               <button
                 type="submit"
-                className="bg-sky-900 rounded-3xl text-white text-xl font-extrabold py-4 px-20"
+                className={`bg-sky-900 rounded-3xl text-white text-xl font-extrabold py-4 px-20 ${
+                  Object.keys(errors).length > 0
+                    ? 'cursor-not-allowed grayscale'
+                    : ''
+                }`}
               >
                 Login
               </button>
